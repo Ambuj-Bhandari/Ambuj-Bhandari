7c321e776af680b683a393a0791ede7896784542
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-04-06 16:24:50 +0530

    Update Jagged.java

--- Jagged.java
+++ Jagged.java
@@ -4,7 +4,7 @@ class Jagg
         
 	public static void main(String args[])
 	{
-		int a[][]=new int[3];
+		int a[][]=new int[3][];
 		a[0]=new int[3];
 		a[1]=new int[4];
 		a[2]=new int[2]; 
@@ -25,10 +25,9 @@ class Jagg
 		{
 			for(int j=0;j<a[i].length;j++)
 			{
-				System.out.print(a[i][j]);
+				System.out.print(a[i][j]+" ");
 			} 
 			System.out.println();
 		} 
 	}
 }
-
