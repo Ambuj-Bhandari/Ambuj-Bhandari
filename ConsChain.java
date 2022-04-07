b7c36a5b0caff84e71eea0eaed31850490515e87
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-04-07 14:32:55 +0530

    Update ConsChain.java

--- ConsChain.java
+++ ConsChain.java
@@ -1,60 +1,63 @@
 import java.util.*;
 class Student
 {
-	String name,Branch,city,university;
 	long id;
+	String Name,Branch,city,university; 
 	Student(String a)
 	{
-		name=a;
-	}
+		Name=a; 
+		System.out.println("Constructor With One Argument\n");
+	} 
 	Student(String a,String b)
 	{
-		this.(a);
-		Branch=b;
-	}
+		this(a);
+		Branch=b; 
+		System.out.println("Constructor With Two Argument\n");
+	} 
 	Student(String a,String b,String c)
 	{
-		this.(a,b);
+		this(a,b);
 		city=c;
-	}
+		System.out.println("Constructor With Three Argument\n");
+	} 
 	Student(String a,String b,String c,String d)
 	{
-		this.(a,b,c);
+		this(a,b,c);
 		university=d;
-	}
+		System.out.println("Constructor With Four Argument\n");
+	} 
 	Student(String a,String b,String c,String d,long e)
 	{
-		this.(a,b,c,d);
+		this(a,b,c,d);
 		id=e;
-	}
+		System.out.println("Constructor With Five Argument\n");
+	} 
 	void display()
 	{
-		System.out.println("Student Name: "+name);
-		System.out.println("Student ID: "+id);
-		System.out.println("Branch: "+Branch);
-		System.out.println("UNIVERSITY: "+university);
+		System.out.println("Name of Student: "+Name);
+		System.out.println("ID of Student: "+id);
+		System.out.println("Branch of Student: "+Branch);
 		System.out.println("City: "+city);
+		System.out.println("University: "+university);
 	}
 	public static void main(String args[])
 	{
-		Scanner in= new Scanner(System.in);
-		System.out.println("Enter the Name of Student");
-		String a= in.nextLine();
+		Scanner in =new Scanner(System.in); 
+		System.out.println("Enter the name of Student");
+		String a=in.nextLine();
 
-		System.out.println("Enter the ID of Student");
-		long b=in.nextLong();
+		System.out.println("Enter the Branch of Sudent");
+		String b=in.nextLine();
+		System.out.println("Enter the city");
+		String c=in.nextLine(); 
 
-		System.out.println("Enter the Branch of Student");
-		String c=in.nextLine();
-
-		System.out.println("Enter the  City");
+		System.out.println("Enter the name of University");
 		String d=in.nextLine();
+		System.out.println("Enter the Student ID");
+		long e=in.nextLong();
 
-		System.out.println("Enter the University Name");
-		String e=in.nextLine();
-
-		Student s=new Student(a,c,d,e,b);
+		Student s=new Student(a,b,c,d,e); 
+		System.out.println();
 		s.display();
 	}
-
 }
