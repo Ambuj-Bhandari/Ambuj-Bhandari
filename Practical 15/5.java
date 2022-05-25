1c3b3a5b9e6f58f835d27f1bd07e419efab200d6
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-05-25 16:05:29 +0530

    Update 5.java

--- Practical 15/5.java
+++ Practical 15/5.java
@@ -1,21 +1,18 @@
 import java.io.*;
-class fivow
+class FIS3
 {
 	public static void main(String args[])throws IOException
 	{
-		FileInputStream fis=new FileInputStream("file1.txt");
-		BufferedInputStream bis=new BufferedInputStream(fis);
+		FileInputStream fis=new FileInputStream("file1.txt"); 
+		FileOutputStream fos=new FileOutputStream("file2.txt");
 		int i=0;
-		System.out.println("The Vowels are: ");
-		while((i=bis.read())!=-1)
-		{ 
-			char ch=(char)i;
-			if(ch=='a'||ch=='e'||ch=='u'||ch=='i'||ch=='o'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-				System.out.print(ch+" ");  
+		while((i=fis.read())!=-1)
+		{
+			fos.write(i);  
 		} 
 		System.out.println();
-		bis.close();
-		fis.close();
+		fis.close(); 
+		fos.close();
 
 		
 	}
