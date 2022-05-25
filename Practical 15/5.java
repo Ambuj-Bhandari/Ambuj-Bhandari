6aa9abe374070a2f75460bb2dfee338b68ea9565
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-05-25 16:04:16 +0530

    Add files via upload

--- Practical 15/5.java
+++ Practical 15/5.java
@@ -1,21 +1,20 @@
 import java.io.*;
-class RW
+class fivow
 {
 	public static void main(String args[])throws IOException
 	{
 		FileInputStream fis=new FileInputStream("file1.txt");
-		BufferedInputStream bis=new BufferedInputStream(fis); 
-
-		FileOutputStream fos=new FileOutputStream("file2.txt");
-		BufferedOutputStream bos=new BufferedOutputStream(fos); 
+		BufferedInputStream bis=new BufferedInputStream(fis);
 		int i=0;
+		System.out.println("The Vowels are: ");
 		while((i=bis.read())!=-1)
-		{
-			bos.write(i);  
+		{ 
+			char ch=(char)i;
+			if(ch=='a'||ch=='e'||ch=='u'||ch=='i'||ch=='o'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+				System.out.print(ch+" ");  
 		} 
-		bos.close();
+		System.out.println();
 		bis.close();
-		fos.close();
 		fis.close();
 
 		
