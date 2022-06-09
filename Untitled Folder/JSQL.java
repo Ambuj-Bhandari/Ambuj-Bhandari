efef89a5ad6669389c5358566057ea992b190691
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-06-09 11:39:59 +0530

    Update JSQL.java

--- Untitled Folder/JSQL.java
+++ Untitled Folder/JSQL.java
@@ -3,7 +3,8 @@ class A
 {
 	public static void main(String args[])
 	{
-		Connection con=null;
+		Connection con=null; 
+		ResultSet rs;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Access Granted!!!!!!!!");
@@ -19,17 +20,43 @@ class A
 		catch(Exception e)
 		{
 			System.out.println("Connection Error");
-		} 
+		}  
+		System.out.println("Entering the Records");
 		try{
 			Statement st=con.createStatement();
-			//st.executeUpdate("insert into ambuj(id,name) values(2,'Divyanshu')");
-			ResultSet rs=st.executeQuery("select * from ambuj");
+			st.executeUpdate("insert into Student(Roll_No,Name) values(01,'Ambuj Bhandari')");
+			st.executeUpdate("insert into Student(Roll_No,Name) values(02,'Divyanshu negi')");
+			st.executeUpdate("insert into Student(Roll_No,Name) values(03,'Abhishek Rana ')"); 
+	
+			System.out.println("Fetching the records:");
+			rs= st.executeQuery("select * from Student");
 			while(rs.next())
 			{
 				System.out.print(rs.getInt(1)+"   ");
 				System.out.print(rs.getString(2));
 				System.out.println();
-			}
+			} 
+			System.out.println("Modifying The records:");
+			st.executeUpdate("update Student set Name='Ambuja Cement' where Roll_No=01");  
+			System.out.println("Record Modified!!!!\n");
+			rs= st.executeQuery("select * from Student");
+			while(rs.next())
+			{
+				System.out.print(rs.getInt(1)+"   ");
+				System.out.print(rs.getString(2));
+				System.out.println();
+			}  
+			System.out.println("\nDeleting The records:");
+			st.executeUpdate("delete from Student where Roll_No=03");  
+			System.out.println("Record Deleted!!!!\n");
+			rs= st.executeQuery("select * from Student");
+			while(rs.next())
+			{
+				System.out.print(rs.getInt(1)+"   ");
+				System.out.print(rs.getString(2));
+				System.out.println();
+			}  
+			
 		}
 		catch(Exception e)
 		{
