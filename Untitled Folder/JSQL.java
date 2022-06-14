14df463608cc81fceb94e619769aeb61d571196f
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-06-14 11:12:25 +0530

    Update JSQL.java

--- Untitled Folder/JSQL.java
+++ Untitled Folder/JSQL.java
@@ -24,8 +24,10 @@ class A
 		catch(Exception e)
 		{
 			System.out.println("Connection Error");
-		}  
-        System.out.println("Enter 1. To enter the records\n2.To fetch records from Table\n3. To modify the records\n4. To delete the records\n");
+		}   
+		do
+		{
+        System.out.println("Enter\n1. To enter the records\n2.To fetch records from Table\n3. To modify the records\n4. To delete the records\n 5. To Exit\n");
 		System.out.println("Enter your choice:");
         ch=in.nextInt();
         switch(ch)
@@ -34,7 +36,8 @@ class A
 			            PreparedStatement ps=con.prepareStatement("insert into Student(Roll_No,Name)values(?,?)");
 			            System.out.println("Enter the Roll No:");
                         r=in.nextInt();
-                        System.out.println("Enter the Name:");
+                        System.out.println("Enter the Name:"); 
+                        in.nextLine();
                         s=in.nextLine(); 
 
                         ps.setInt(1,r);
@@ -44,8 +47,10 @@ class A
             catch(Exception er)
             {
                 System.out.println("Issue in inserting!!");
-            } 
+            }  
+            System.out.println();
             break; 
+            
             case 2:
 			System.out.println("Fetching the records:");  
             try{
@@ -61,52 +66,70 @@ class A
             catch(Exception e)
             {
                 System.out.println("Fetching Issues!!");
-            } 
+            }  
+            System.out.println();
             break; 
+            
             case 3:
 			System.out.println("Modifying The records:"); 
             try{ 
-                System.out.println("Enter the name and roll number to be modified:");
-                s=in.nextLine();
-                r=in.nextInt();
+            	in.nextLine();
+                System.out.println("Enter the name to be modified:");
+                
+                String st=in.nextLine(); 
+                
+                System.out.println("Enter the roll No.:");
+                int ro=in.nextInt(); 
                 PreparedStatement ps3=con.prepareStatement("update Student set Name=? where Roll_No=?");
-                ps3.setInt(1, r);
-                ps3.setString(2, s);
-            } 
-            catch(Exception er)
-            {
-                System.out.println("Modifying Issue");
-            }
-			/*rs= st.executeQuery("select * from Student");
+                ps3.setString(1,st);
+                ps3.setInt(2, ro);  
+                ps3.executeUpdate();
+                
+           
+            PreparedStatement p2=con.prepareStatement("select * from Student");
+			rs= p2.executeQuery();
 			while(rs.next())
 			{
 				System.out.print(rs.getInt(1)+"   ");
 				System.out.print(rs.getString(2));
 				System.out.println();
-			}*/ 
+			} 
+			} 
+            catch(Exception er)
+            {
+                System.out.println("Modifying Issue");
+            } 
+            System.out.println();
             break; 
             case 4:
 			System.out.println("Enter the Roll no. to be deleted");
             r=in.nextInt(); 
             try{
             PreparedStatement ps4=con.prepareStatement("delete from Student where Roll_No=?");
-            ps4.setInt(1, r); 
-            }
-			/*rs= st.executeQuery("select * from Student");
+            ps4.setInt(1, r);
+            ps4.executeUpdate(); 
+            
+            PreparedStatement p3=con.prepareStatement("select * from Student");
+			rs= p3.executeQuery();
 			while(rs.next())
 			{
 				System.out.print(rs.getInt(1)+"   ");
 				System.out.print(rs.getString(2));
 				System.out.println();
-			}*/
-		catch(Exception e)
-		{
+			} 
+			} 
+            catch(Exception e)
+			{
 			System.out.println("Deleting Issue!!!");
-		} 
+			} 
+			System.out.println();
+		
         break; 
-        default:
-        System.exit(0); 
-    }
+        case 5:System.out.println("bye Bye!!!"); 
+        		break;
+        default:System.out.println("Invalid Choice!!!"); 
+        }
+    } while(ch!=5); 
 		
 	}
 }
